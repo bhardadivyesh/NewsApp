f19bc7882a1e793eb4fdf042898b5cbb34230937
Bharda Divyesh <bhardadivyesh9@gmail.com>
2023-12-31 17:39:11 +0530

    [DB][sign in page login object changes]

--- src/Component/Login/SignIn.jsx
+++ src/Component/Login/SignIn.jsx
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 function Login() {
   const [data, setData] = useState({
     email: "",
+    'gttmgiotjgiotgjtoijhgtiojgio
     password: "",
   });
   const navigate = useNavigate();
